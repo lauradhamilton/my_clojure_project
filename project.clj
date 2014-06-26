0d2c4e8cc087185b99771b057ff43b49585f07cb
Laura Hamilton <lauradhamilton@gmail.com>
2014-06-25 19:04:44 -0500

    Committing first Clojure project

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject my_clojure_project "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :main ^:skip-aot my-clojure-project.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
---
+++ src/my_clojure_project/core.clj
@@ -0,0 +1,7 @@
+(ns my-clojure-project.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
